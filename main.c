5ed426a6d681d8cc30b698af1d46e6b4d3a84fd6
Valerii Rodtsevych <vrodtsevych@hotmail.com>
2024-11-21 22:22:32 -0500

    Create structure of program

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+//Purpose of program: track daily advancements in user-specified directions of life. Score based system.
+
+
+
+// Take flags as input for CRUD operations
+// -show (all, specific)
+// -new (database name.sqlite)
+// -create (new metric) 
+// -delete (metric, or database)
+// -modify (name of metric, description, or name)
+int main(int argc, char *argv[]) {
+    
+
+    return 0;
+}
